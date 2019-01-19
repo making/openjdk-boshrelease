1c992e2b20144acccf3895a030eea65883be5c55
Toshiaki Maki <tmaki@pivotal.io>
2019-01-19 20:17:53 +0900

    Update add-blobs.sh

--- add-blobs.sh
+++ add-blobs.sh
@@ -9,7 +9,7 @@ mkdir -p .downloads
 cd .downloads
 
 
-JDK_VERSION=1.8.0_192
+JDK_VERSION=1.8.0_202
 
 if [ ! -f ${DIR}/blobs/java/openjdk-${JDK_VERSION}.tar.gz ];then
     curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-${JDK_VERSION}.tar.gz
