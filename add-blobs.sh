f1c2cfa19883c0b622278ef2f5dd3310e9cf8a3d
Toshiaki Maki <tmaki@pivotal.io>
2019-04-21 23:22:27 +0900

    Bump to JDK 11.0.3_07

--- add-blobs.sh
+++ add-blobs.sh
@@ -9,11 +9,11 @@ mkdir -p .downloads
 cd .downloads
 
 
-JDK_VERSION=11.0.2_09
+JDK_VERSION=11.0.3_07
 
 if [ ! -f ${DIR}/blobs/java/openjdk-${JDK_VERSION}.tar.gz ];then
-    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-${JDK_VERSION}.tar.gz
-    bosh add-blob --dir=${DIR} openjdk-${JDK_VERSION}.tar.gz java/openjdk-${JDK_VERSION}.tar.gz
+    curl -L -O -J https://download.run.pivotal.io/openjdk-jdk/bionic/x86_64/openjdk-jdk-${JDK_VERSION}-bionic.tar.gz
+    bosh add-blob --dir=${DIR} openjdk-jdk-${JDK_VERSION}-bionic.tar.gz java/openjdk-${JDK_VERSION}.tar.gz
 fi
 
 cd -
